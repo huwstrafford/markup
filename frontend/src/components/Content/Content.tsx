34de7b89568f6cc374adcdacfb4d929a33d2acc9
Samuel Dobbie <samuel.jordan.dobbie@gmail.com>
2021-03-23 23:43:17 +0000

    fixup!

---
+++ frontend/src/components/Content/Content.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import './Content.css'
+
+function Content() {
+  return (
+    <main>
+      <div id="intro" className="view">
+        <div className="mask"></div>
+      </div>
+    </main>
+  )
+}
+
+export default Content
---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+function Footer() {
+  return (
+    <footer>
+      Hello from footer
+    </footer>
+  )
+}
+
+export default Footer
