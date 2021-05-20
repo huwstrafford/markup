1d8d421197ba15048b84d4eb4bfda0b7ce642ea1
Samuel Dobbie <samuel.jordan.dobbie@gmail.com>
2021-05-20 01:56:48 +0100

    Add skeleton for marking highlighted document text

--- frontend/src/components/Annotate/AnnotationPanel/AnnotationPanel.tsx
+++ frontend/src/components/Annotate/AnnotationPanel/AnnotationPanel.tsx
@@ -1,8 +1,10 @@
-import { useState, useEffect } from "react"
+import { useEffect, useState } from "react"
 import Scrollbars from "react-custom-scrollbars-2"
 
-import "./AnnotationPanel.css"
 import AnnotationPrediction from "./AnnotationPrediction/AnnotationPrediction"
+import Annotation from "./Helper/Annotation"
+import "./AnnotationPanel.css"
+
 
 interface Annotation {
   entity: string,
@@ -37,7 +39,7 @@ function AnnotationPanel(props: any): JSX.Element {
           <AnnotationPrediction/>
         
           {annotations.map((annotation: Annotation, index: number) => {
-            return <p className="annotation">{annotation.message}</p>
+            return <Annotation/>
           })}
         </div>
       </Scrollbars>
--- frontend/src/components/Annotate/DocumentPanel/DocumentPanel.tsx
+++ frontend/src/components/Annotate/DocumentPanel/DocumentPanel.tsx
@@ -12,12 +12,33 @@ function DocumentPanel(props: any): JSX.Element {
   return (
     <div className="panel">
       <Scrollbars autoHide>
-        <div className="panel-content document-text">
+        <div
+          id="document-text"
+          className="panel-content document-text"
+          onMouseUp={(e) => markText()}
+        >
           {documentText}
         </div>
       </Scrollbars>
     </div>
   )
+}
+
+function markText(): void {
+  if (document.getElementsByTagName("mark").length > 0) {
+    // TODO remove mark
+    return
+  }
+
+  const selectionText = window.getSelection()!.toString().trim()
+  const selectionRange = window.getSelection()!.getRangeAt(0)
+
+  if (selectionText.length === 0) {
+    return 
+  }
+  
+  const newNode = document.createElement("mark");
+  selectionRange.surroundContents(newNode)
 }
 
 export default DocumentPanel
