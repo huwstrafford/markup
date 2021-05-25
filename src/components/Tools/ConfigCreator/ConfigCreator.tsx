2a365c6b42a20528a9c789de0d13878695730b8b
Samuel Dobbie <samuel.jordan.dobbie@gmail.com>
2021-05-25 22:55:41 +0100

    Update data generator display

--- src/components/Tools/ConfigCreator/ConfigCreator.tsx
+++ src/components/Tools/ConfigCreator/ConfigCreator.tsx
@@ -1,63 +1,36 @@
+import { MDBBtn, MDBCol, MDBRow } from "mdbreact"
+
 import "./ConfigCreator.css"
 
 function ConfigCreator(): JSX.Element {
   return (
-    <div className="container">
-      <div className="config-panel">
-        <div className="option-container">
-            <p className="option-headline option-item">
-                Entities
-                <span className="config-tooltip" title="The entity category that you want to select (e.g. Seizure).">
-                    <i className="far fa-question-circle"></i>
-                </span>
-            </p>
-            <div className="option-item">
-                <input id="entity-name" className="option-input-field" type="text" placeholder="Entity name (e.g. Seizure)"/>
-            </div>
-            <div className="option-item">
-                <label id="add-entity" className="option-button option-button-small">+ Entity</label>
-            </div>
-        </div>
+    <div className="config-creator-container">
+      <MDBRow>
+        <MDBCol md="12">
+          <div className="config-creator-panel">
+            <h3 className="config-creator-header">Entites (i)</h3>
+            <input className="config-creator-input" placeholder="Name (e.g. doseUnit)"/>
+            <MDBBtn className="config-creator-button">Add Variable</MDBBtn>
+          </div>
+        </MDBCol>
 
-        <div className="option-container">
-            <p className="option-headline option-item">
-                Attributes
-                <span className="config-tooltip" title="The attributes you want an entity to have (e.g. SeizureFrequency).">
-                    <i className="far fa-question-circle"></i>
-                </span>
-            </p>
-            <div className="option-item">
-                <input id="attribute-name" className="option-input-field" type="text" placeholder="Attribute name (e.g. SeizureFrequency)"/>
-            </div>
-            <div className="option-item">
-                <input id="attribute-relation" className="option-input-field" type="text" placeholder="Related entity (e.g. Seizure)"/>
-            </div>
-            <div className="option-item">
-                <input id="attribute-dropdown" className="option-input-field" type="text" placeholder="Dropdown values (e.g. Daily, Weekly, Monthly)"/>
-            </div>
-            <div className="option-item">
-                <label id="add-attribute" className="option-button option-button-small">+ Attribute</label>
-            </div>
-        </div>
+        <MDBCol md="12">
+          <div className="config-creator-panel">
+            <h3 className="config-creator-header">Attributes (i)</h3>
+            <input className="config-creator-input" placeholder="Input sequence (e.g. She is taking $drugDose $doseUnit of $drugName.)"/>
+            <input className="config-creator-input" placeholder="Target sequence (e.g. dose: $drugDose; unit: $doseUnit; name: $drugName;)"/>
+            <MDBBtn className="config-creator-button">Add Template</MDBBtn>
+          </div>
+        </MDBCol>
+      </MDBRow>
 
-        <div className="option-container">
-            <p className="option-headline option-item">
-                Output
-                <span className="config-tooltip" title="The entities and attributes that will be included in the exported configuration file.">
-                    <i className="far fa-question-circle"></i>
-                </span>
-            </p>
-            <span id="empty-container-message">Nothing added yet.</span>
-            <div id="output-list-container">
-                <div id="entity-list" className="option-column"><span>Added Entities</span></div>
-                <div id="attribute-list" className="option-column"><span>Added Attributes</span></div>
-            </div>
-        </div>
-
-        <div className="completed-option-container">
-            <a href="/" id="save-configuration-file" className="option-button">Export Config File</a>
-        </div>
-      </div>
+      <MDBRow>
+        <MDBCol>
+          <div className="config-creator-panel config-creator-panel-small">
+            <MDBBtn>Export Configs</MDBBtn>
+          </div>
+        </MDBCol>
+      </MDBRow>
     </div>
   )
 }
--- src/components/Tools/DataGenerator/DataGenerator.tsx
+++ src/components/Tools/DataGenerator/DataGenerator.tsx
@@ -6,42 +6,20 @@ function DataGenerator(): JSX.Element {
   return (
     <div className="data-generator-container">
       <MDBRow>
-        <MDBCol>
+        <MDBCol md="12">
           <div className="data-generator-panel">
-            <h3>Variables (i)</h3>
-
-            <p>
-              <select>
-                <option value="1">Text values</option>
-                <option value="2">Numerical range</option>
-              </select>
-            </p>
-
-            <p>
-              <input className="data-generator-input" placeholder="Name (e.g. doseUnit)"/>
-            </p>
-
-            <p>
-              <input className="data-generator-input" placeholder="Comma-seperate values (e.g. grams, milligrams)"/>
-            </p>
-
-            <MDBBtn className="data-generator-button">+ Variable</MDBBtn>
+            <h3 className="data-generator-header">Variables (i)</h3>
+            <input className="data-generator-input" placeholder="Name (e.g. doseUnit)"/>
+            <MDBBtn className="data-generator-button">Add Variable</MDBBtn>
           </div>
         </MDBCol>
 
-        <MDBCol>
+        <MDBCol md="12">
           <div className="data-generator-panel">
-            <h3>Sequence Templates (i)</h3>
-
-            <p>
-              <input className="data-generator-input" placeholder="Input sequence (e.g. She is taking $drugDose $doseUnit of $drugName.)"/>
-            </p>
-
-            <p>
-              <input className="data-generator-input" placeholder="Target sequence (e.g. dose: $drugDose; unit: $doseUnit; name: $drugName;)"/>
-            </p>
-
-            <MDBBtn className="data-generator-button">+ Template</MDBBtn>
+            <h3 className="data-generator-header">Sequence Templates (i)</h3>
+            <input className="data-generator-input" placeholder="Input sequence (e.g. She is taking $drugDose $doseUnit of $drugName.)"/>
+            <input className="data-generator-input" placeholder="Target sequence (e.g. dose: $drugDose; unit: $doseUnit; name: $drugName;)"/>
+            <MDBBtn className="data-generator-button">Add Template</MDBBtn>
           </div>
         </MDBCol>
       </MDBRow>
@@ -49,9 +27,21 @@ function DataGenerator(): JSX.Element {
       <MDBRow>
         <MDBCol>
           <div className="data-generator-panel data-generator-panel-small">
-            Hello
+            <div className="data-generator-quantity-container">
+              <p className="data-generator-quantity-text">25,000 sequences</p>
+              <input
+                className="data-generator-quantity-range"
+                type="range"
+                min="0"
+                max="50000"
+                step="100"
+                value="25000"
+              />
+            </div>
             
-            <MDBBtn>Generate Data</MDBBtn>
+            <div className="data-generator-output-container">
+              <MDBBtn className="data-generator-output-button">Generate Data</MDBBtn>
+            </div>
           </div>
         </MDBCol>
       </MDBRow>
