1fff8547db46bad2e0f61bda106dc07f309a307a
Huw <h.a.strafford@swansea.ac.uk>
2022-11-25 14:05:05 +0000

    Update for Django 4.0.0

--- annotate/urls.py
+++ annotate/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url
+from django.urls import include, path, re_path
 
 from . import views
 
 urlpatterns = [
-    url('suggest-cui/', views.suggest_cui, name='suggest_cui'),
-    url('suggest-annotations/', views.suggest_annotations, name='suggest_annotations'),
-    url('teach-active-learner/', views.teach_active_learner, name='teach_active_learner'),
-    url('reset-ontology/', views.reset_ontology, name='reset_ontology'),
-    url('setup-demo/', views.setup_demo, name='setup_demo'),
-    url('setup-umls/', views.setup_umls, name='setup_umls'),
-    url('setup-preloaded-ontology/', views.setup_preloaded_ontology, name='setup_preloaded_ontology'),
-    url('setup-custom-ontology/', views.setup_custom_ontology, name='setup_custom_ontology'),
-    url('', views.annotate_data, name='annotate_data'),
+    re_path('suggest-cui/', views.suggest_cui, name='suggest_cui'),
+    re_path('suggest-annotations/', views.suggest_annotations, name='suggest_annotations'),
+    re_path('teach-active-learner/', views.teach_active_learner, name='teach_active_learner'),
+    re_path('reset-ontology/', views.reset_ontology, name='reset_ontology'),
+    re_path('setup-demo/', views.setup_demo, name='setup_demo'),
+    re_path('setup-umls/', views.setup_umls, name='setup_umls'),
+    re_path('setup-preloaded-ontology/', views.setup_preloaded_ontology, name='setup_preloaded_ontology'),
+    re_path('setup-custom-ontology/', views.setup_custom_ontology, name='setup_custom_ontology'),
+    re_path('', views.annotate_data, name='annotate_data'),
 ]
