1ff8ba7919d9f59ac90d1719a8a182d4f975c603
Huw <h.a.strafford@swansea.ac.uk>
2022-12-02 15:15:21 +0000

    Fixed issue with not Epilpesy letters not working

--- compare/urls.py
+++ compare/urls.py
@@ -5,9 +5,13 @@ from . import views
 urlpatterns = [
     path('', views.compare, name='compare'),
     path('iaa/', views.inter_annotator_agreement, name='iaa'),
-    path('iaa/run-IAA/', views.runEverythingWithRemove, name='run-IAA'),
-    path('iaa/run-IAA-all/', views.runEverything2WithRemove, name='run-IAA'),
-    path('iaa/run-IAA-entities/', views.get_f1score_per_annotationCorpusHTMLWithRemove, name='run-IAA'),
-    path('iaa/view-Difference-Only/', views.getDiffAnnfileWithRemove, name='run-IAA'),
+    # path('iaa/run-IAA/', views.runEverythingWithRemove, name='run-IAA'),
+    # path('iaa/run-IAA-all/', views.runEverything2WithRemove, name='run-IAA'),
+    # path('iaa/run-IAA-entities/', views.get_f1score_per_annotationCorpusHTMLWithRemove, name='run-IAA'),
+    # path('iaa/view-Difference-Only/', views.getDiffAnnfileWithRemove, name='run-IAA'),
+    path('iaa/run-IAA/', views.runEverything, name='run-IAA'),
+    path('iaa/run-IAA-all/', views.runEverything2, name='run-IAA'),
+    path('iaa/run-IAA-entities/', views.get_f1score_per_annotationCorpusHTML, name='run-IAA'),
+    path('iaa/view-Difference-Only/', views.getDiffAnnfile, name='run-IAA'),
     path('iaa/entities-and-annotation-all/', views.entities_and_annotation_all, name='run-IAA'),
 ]
